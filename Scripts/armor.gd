82321fcf79446a1ea1a497b5fbf66260d13cba08
Lendevar <lendevar@gmail.com>
2021-07-23 17:01:52 +0300

    Added armor and penetration for future testing

---
+++ Scripts/armor.gd
@@ -0,0 +1,8 @@
+extends StaticBody
+
+export(int) var armorThickness
+
+func _ready():
+	pass # Replace with function body.
+
+
--- gameController.gd
+++ gameController.gd
@@ -13,6 +13,7 @@ class bullet:
 	var speed = 1
 	var model
 	var trajectory
+	var penetration = 35
 
 class enemy:
 	var maxhp = 50
@@ -31,7 +32,6 @@ func _ready():
 		
 		arrayEnemiesModels += [newEnemy.model]
 		arrayEnemies += [newEnemy]
-
 
 func testShooting(muzzle, target):
 	var newBullet = bullet.new()
@@ -54,13 +54,30 @@ func bulletHitEnemyNoArmor(bullet, enemy):
 		arrayEnemies.erase(enemy)
 		remove_child(enemy.model)
 
+func bulletHitArmoredEnemy(bullet, enemy, armor, center, normal):
+	var plateVector = normal.global_transform.origin - center.global_transform.origin
+	plateVector = plateVector.normalized()
+	
+	var angle = rad2deg(bullet.trajectory.angle_to(plateVector))
+	if angle > 90:
+		angle = 180 - angle
+	angle = 90 - angle
+	
+	var efficientArmor = armor.armorThickness / cos(deg2rad(90 - angle))
+	var currentEnemy = enemy
+	
+	if bullet.penetration >= efficientArmor:
+		print("Penetration! Bullet pen = ", bullet.penetration, " eff armor = ", efficientArmor)
+		bulletHitEnemyNoArmor(bullet, currentEnemy)
+	else:
+		print("Didn't go through!")
+	
+
 func moveFlyingBullets():
 	var arrayToErase = []
 	
 	for bullet in arrayFlyingBullets:
-		
 		var bulletCollision = bullet.model.move_and_collide(bullet.trajectory * bullet.speed)
-		
 		if bulletCollision != null:
 			var collider = bulletCollision.get_collider()
 			
@@ -72,13 +89,26 @@ func moveFlyingBullets():
 					for enemy in arrayEnemies:
 						if enemy.model == collider:
 							bulletHitEnemyNoArmor(bullet, enemy)
-						
+			
+			if collider.get_class() == "StaticBody":
+				if str(collider.get_path()).find("armor") != -1:
+					var armoredEnemyModel = collider.get_parent()
+					var armorCenter = get_node(str(collider.get_path()) + "/center")
+					var armorNormal = get_node(str(collider.get_path()) + "/normal")
+					
+					var currentEnemy
+					
+					for enemy in arrayEnemies:
+						if enemy.model == armoredEnemyModel:
+							currentEnemy = enemy
+					
+					bulletHitArmoredEnemy(bullet, currentEnemy, collider, armorCenter, armorNormal)
+				
 	
 	for bullet in arrayToErase:
 		arrayFlyingBullets.erase(bullet)
 	
 	arrayToErase.clear()
-
 
 func _process(delta):
 	moveFlyingBullets()
